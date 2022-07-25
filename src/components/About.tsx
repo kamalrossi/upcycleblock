338e7ff39c408ab787c1e400c4dfff1c7282acd3
Rossi Kamal <rossikamal@gmail.com>
2022-07-25 19:38:07 +0600

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -79,9 +79,9 @@ const About = () => {
         </div>
         <div className="flex items-center mt-6">
           <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-            &copy; {new Date().getFullYear()} designed by{' '}
-            <a href="https://github.com/issaafalkattan" rel="nofollow">
-              Issaaf Kattan
+            &copy; {new Date().getFullYear()} All Rights Reserved{' '}
+            <a href="" rel="nofollow">
+           CryptoBlock
             </a>
           </p>
         </div>
